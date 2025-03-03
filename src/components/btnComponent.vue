b0ed11bf451813c8e97963a80ee21183158fc07d
Matilda.Extern <Matilda.Olsson.Extern@skanetrafiken.se>
2025-03-03 08:51:12 +0100

    moved the button to it's own component with props

---
+++ src/components/btnComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="container">
+        <button class="popup" @click="togglePopup">
+            {{ btnText }}
+            <div class="popup-content" ref="popupContent">
+                <img :src="src" :alt="alt" />
+            </div>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BtnComponent',
+    props: {
+        src: String,
+        alt: String,
+        btnText: String
+    },
+    methods: {
+        togglePopup() {
+            const popup = this.$refs.popupContent;
+            if (popup) {
+                popup.classList.toggle('show');
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+@import "../scss/main.scss";
+
+#container {
+    border-width: 0.3rem;
+}
+</style>
