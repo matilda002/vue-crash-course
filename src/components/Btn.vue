07bbf546492dc5dc3c6d8da9f99a631a8104335b
Matilda.Extern <Matilda.Olsson.Extern@skanetrafiken.se>
2025-03-11 09:53:41 +0100

    formated code

--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,37 +1,41 @@
 <template>
-    <button class="popup" @click="handleCount">
-        {{ btnText }}
-        <div class="popup-content" :class="{ show: isPopupVisible }">
-            <img :src="src" :alt="alt" />
-        </div>
-    </button>
+  <button class="popup" @click="handleCount">
+    {{ btnText }}
+    <div
+      class="popup-content"
+      :class="{ show: isPopupVisible }"
+      aria-hidden="true"
+    >
+      <img :src="src" :alt="alt" />
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-    name: "BtnComponent",
-    props: {
-        src: String,
-        alt: String,
-        btnText: String,
-        action: String,
+  name: "BtnComponent",
+  props: {
+    src: String,
+    alt: String,
+    btnText: String,
+    action: String,
+  },
+  data() {
+    return {
+      isPopupVisible: false,
+    };
+  },
+  methods: {
+    handleCount() {
+      this.$emit("update-count", this.action);
+      this.showPopup();
     },
-    data() {
-        return {
-            isPopupVisible: false,
-        };
+    showPopup() {
+      this.isPopupVisible = true;
+      setTimeout(() => {
+        this.isPopupVisible = false;
+      }, 2000);
     },
-    methods: {
-        handleCount() {
-            this.$emit("update-count", this.action);
-            this.showPopup();
-        },
-        showPopup() {
-            this.isPopupVisible = true;
-            setTimeout(() => {
-                this.isPopupVisible = false;
-            }, 2000);
-        },
-    },
+  },
 };
 </script>
